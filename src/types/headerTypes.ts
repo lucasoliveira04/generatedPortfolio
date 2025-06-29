d8934698cee7098fd36002307a88bdbce7eada7c
= <lucasolisocialmedia@gmail.com>
2025-06-28 21:10:05 -0800

    feat: props header

---
+++ src/types/headerTypes.ts
@@ -0,0 +1,11 @@
+export type HeaderProps = {
+    title: string;
+    textColorTitle?: string;
+    subtitle?: string;
+    textColorSubtitle?: string;
+    backgroundColor?: string;
+    textColor?: string;
+
+    props?: React.HTMLAttributes<HTMLDivElement>;
+
+}
